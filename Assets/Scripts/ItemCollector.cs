bfeedf4e8a8595b79ab278bc9cf41753d7e36a3c
sultanrosh <krashidi@purdue.edu>
2022-05-29 22:32:09 -0400

    New Item

---
+++ Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCollector : MonoBehaviour
+{
+    int coins = 0;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            Destroy(other.gameObject);
+            coins++;
+        }
+    }
+}
